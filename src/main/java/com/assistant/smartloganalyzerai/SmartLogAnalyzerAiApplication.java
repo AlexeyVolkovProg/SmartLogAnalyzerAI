b46cfd7572ab923d2111ce6f6fd04e9105193a03
Alexey Volkov <alexvolkovprogrammer@yandex.com>
2026-02-12 19:49:28 +0300

    Initial Commit

---
+++ src/main/java/com/assistant/smartloganalyzerai/SmartLogAnalyzerAiApplication.java
@@ -0,0 +1,13 @@
+package com.assistant.smartloganalyzerai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartLogAnalyzerAiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartLogAnalyzerAiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assistant/smartloganalyzerai/SmartLogAnalyzerAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assistant.smartloganalyzerai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartLogAnalyzerAiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
